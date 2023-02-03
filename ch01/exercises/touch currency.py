98ddd6d54f8c34bd1f499ab4c5b6290a61633396
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-02-03 10:30:01 -0500

    update

---
+++ ch01/exercises/touch currency.py
@@ -0,0 +1,2 @@
+import rate 
+var = input(rate)
--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,13 @@
-
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition/classes)/weeks)
+print ("Cost per week:", cost_per_week)
+Weeks = 16 
+classes = 48 
+classes_per_week = (classes/weeks)
+print ("Classes_per_week:", classes_per_week)
+cost_per_class = (cost_per_week/classes_per_week)
+print ("Cost per class:",cost_per_class)
+print ("have a great semester!")
