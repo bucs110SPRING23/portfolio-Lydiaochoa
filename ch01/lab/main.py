97098437217a94600106e8a1856fb46a1c3748ab
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-02-06 11:17:55 -0500

    Update

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,3 +1,4 @@
+import random
 #Part A
 weeks = 16
 print(weeks, type(weeks))
@@ -6,16 +7,13 @@ print(classes, type(classes))
 tuition = 6000
 print(tuition, type(tuition))
 cost_per_week = ((tuition/classes)/weeks)
-print ("Cost per week:", cost_per_week)
-Weeks = 16 
-classes = 48 
-classes_per_week = (classes/weeks)
-print ("Classes_per_week:", classes_per_week)
+print ("Cost per week:", cost_per_week, (type(cost_per_week)))
+classes_per_week = 3
+print ("Classes per week:", classes_per_week, type(classes_per_week))
 cost_per_class = (cost_per_week/classes_per_week)
-print ("Cost per class:",cost_per_class)
-print ("have a great semester!")
-import random
+print ("Cost per class:",cost_per_class, type(cost_per_class))
+
+
 mylist = [1,2,3,4,5,6,7,8,9,10]
-random.choice(mylist)
-print(random.choice(mylist))
-#not pushing to github
+print("your random number is", random.choice(mylist))
+
