223723e3a266635d961de158879ee95ca401f19f
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-02-08 20:16:24 -0500

    changes

---
+++ ch02/exercises/Shapes.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init() 
+
+screen = pygame.display. set_mode()
+screen.fill ("red")
+pygame.display.flip()
+input("press enter to continue")
+screen.fill ("green")
+pygame.display.flip
+input("press enter to continue")
+#flip keeps track of display imgage and next image
+#code not working. why?
