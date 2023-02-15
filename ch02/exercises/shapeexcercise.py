e8c28dbb5765dd73af5f93803301838a4b8303fd
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-02-15 15:41:34 -0500

    checking

--- ch02/exercises/shapeexcercise.py
+++ ch02/exercises/shapeexcercise.py
@@ -11,9 +11,9 @@ while 1:
     dimensions = [screen_size[0] / 2, screen_size[1] / 2]
     screen.fill("white")
     pygame.time.wait(1000) #do I need wait function for this 
-    pygame.draw.circle(screen,"blue", dimensions, 50)
-    pygame.draw.circle(screen,"blue", [100,100], 75)
-    pygame.draw.circle(screen,"blue", [150,150], 100)
+    pygame.draw.circle(screen,"blue", [850,480], 50)
+    pygame.draw.circle(screen,"blue", [850,600], 75)
+    pygame.draw.circle(screen,"blue", [850,750], 100)
     pygame.display.flip()
     pygame.time.wait(1000)
     break
