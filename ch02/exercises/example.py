5c427bf91b925eb446cebb7975ae07212e832cb1
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-02-20 11:28:31 -0500

    update

--- ch02/exercises/example.py
+++ ch02/exercises/example.py
@@ -23,3 +23,25 @@ window.exitonclick()
 
 
 #window must always be last turtle command 
+#part B
+pygame.init()
+window = pygame.display.set_mode
+color = ("blue")
+pointslist = [] #can i just put points in like this 
+num_sides = (int(input("enter number of sides:")))
+side_length = (input("enter legnth of sides"))
+xpos = 
+ypos = 
+for in s (number_of_sides)
+    angle= 360/number_of_sides
+    radians = math.radians(angle * i)
+    x =xpos + side_length * math.cos(radians)
+    y = ypos + sde_length * math.sin(radians)
+    break  
+pygame.draw.polygon(pointslist,color)
+color = ("blue")
+pygame.display.flip 
+pygame.time.wait(1000)
+window.fill("white")
+pygame.display.flip 
+window.exitonclick
--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,3 +1,34 @@
 import random
+import pygame
+import turtle
+import math
 
+#Part A
+window = turtle.Screen()
+turtle1 = turtle.Turtle
+turtle2 = turtle.Turtle
+turtle1.shape("turtle")
+turtle2.shape("turtle")
+turtle1.color("blue")
+turtle2.color("orange")
+
+#race1
+turtle1.goto(-100,20)
+turtle2.goto(-100,-20)
+turtle1.forward(random.randomrandrange(1,101))
+turtle2.forward(random.randomrandrange(1,101))
+turtle1.goto(-100,20) #is there a way i can make them return to a position without repeating 
+turtle2.goto(-100,20)
+
+#race2
+for r in (0,10):
+    random.randrange(1,11)
+    turtle1.goto(-100,20)
+    turtle2.goto(-100,-20)
+    turtle1.forward(randomrandrange) #can i use period for this
+    turtle2.forward(randomrandrange)
+    turtle1.position(-100,20)
+    turtle2.position(-100,20)
+    break
+window.exitonclick()
 
