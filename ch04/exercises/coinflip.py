77bc2fad80dca5a98eab56f992ee87a2665da9fb
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-02-22 16:53:21 -0500

    2/22

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,20 @@
+import random
+import turtle 
+window = turtle.Screen()
+turtle1 = turtle.Turtle()
+turtle1.shape("turtle")
+turtle1.color("pink")
+
+coin = random.randomint(0,1)
+if coin == 1:
+    turtle1.left(90)
+    turtle1.forward(20)
+
+if coin == 0:
+    turtle1.right(90)
+    turtle1.forward(20)
+
+window.exitonclick()
+
+
+
