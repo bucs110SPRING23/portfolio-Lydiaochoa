7395dbc2f9cbd5639bb74cc22f54e798814759e9
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-03-06 17:57:43 -0500

    update

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -33,4 +33,57 @@ while is_in_screen():
 window.exitonclick()
 
 
+# class notes 3/6 
 
+#def defines a fucntion
+# funtcitons are always defined in  global scope 
+#scope- where the data/ algorithm is accessible 
+# vending machine 
+# black boxing --> writing code that determines something, and then doesnt matter anymore/ we dont care how it works 
+print("Welcome to the vending machine")
+code = input("please enter a code: ")
+money = input("give me money: ")
+
+def my_vending_machine():
+    if code == "A": 
+        if money >= 1:
+            print("you got a coke")
+    elif code == "B":
+        if money >= 1.5:
+            print("you got a water")
+        else: 
+            print("you need more money")
+    elif code == "c":
+        if money >=2:
+            print("you got a juice")
+    else:
+        print("invalid code")
+
+def find_max(x, y, z): #def can acces varibles in a global scope: basically, def can provide a fucntion, but unless we run the code or input something, it doesnt get use 
+   #print("please enter 3 numbers: ")
+    #a = int(input(" : "))
+    #b = int(input(" : "))
+    #c = int(input(" : "))
+    max = a 
+    if y > max:
+        max = y 
+    if z > max:
+        max = z
+print(max)
+
+for _ in range(3):
+    print("please enter 3 numbers: ")
+    a = int(input(" : "))
+    b = int(input(" : "))
+    c = int(input(" : "))
+    find_max(a, b)
+# not passing A,b , and c, ut their values as python has assigned them to x y z 
+#collision: 2 variables of the same name 
+# cant have variables of the same name in the same scope 
+def foo(var): #var in fucntion scope assures that 2 different var are not the same--> using the same name as a global variable is called shadowing 
+    var+= 1
+    print(var)
+
+var = 5 
+foo(var)
+print(var)
---
+++ ch05/exercises/printpattern.py
@@ -0,0 +1,8 @@
+def starpyramid():
+    rows = number
+    multiplier = i + 1
+    print(pattern)
+
+for _ in range(4):
+    number = input((print("input number: ")))
+
