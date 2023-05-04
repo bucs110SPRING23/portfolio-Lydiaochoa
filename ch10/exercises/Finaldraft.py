110bd99b0380159094b5e2b2c1a10c8b15ca5de9
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-05-04 12:17:12 -0400

    saved

--- ch10/exercises/Finaldraft.py
+++ ch10/exercises/Finaldraft.py
@@ -1,27 +1,27 @@
 
 import pygame 
-# import tkinter as tk
-# from tkinter import ttk
+import tkinter as tk
+from tkinter import ttk
 
-# global my_counter
+global my_counter
 
 
-# def create_user_interface(application_window):
-#     global my_counter
+def create_user_interface(application_window):
+    global my_counter
 
-#     my_counter = ttk.Label(application_window, text="0")
-#     my_counter.grid(row=0, column=0)
+    my_counter = ttk.Label(application_window, text="0")
+    my_counter.grid(row=0, column=0)
 
-#     class playerbuttons: 
-#         window = tk.Tk()
-#         create_user_interface(window)
-#         player_button = ttk.Button(application_window, text = "jump")
-#         player_button.grid(row=1, column=0)
-#         quit_button = ttk.Button(application_window, text="Quit")
-#         quit_button.grid(row=2, column=0)
-#         quit_button['command'] = window.destroy
+    class playerbuttons: 
+        window = tk.Tk()
+        create_user_interface(window)
+        player_button = ttk.Button(application_window, text = "jump")
+        player_button.grid(row=1, column=0)
+        quit_button = ttk.Button(application_window, text="Quit")
+        quit_button.grid(row=2, column=0)
+        quit_button['command'] = window.destroy
 
-#         window.mainloop()
+        window.mainloop()
 
 pygame.init
 w = 10000
@@ -37,7 +37,10 @@ river_height= (h / 5 ) * 2
 
 pygame.draw.rect(screen,"cadetblue2",(0,400,river_width,river_height))
 pygame.display.flip()
-pygame.draw.circle(screen,"black",[20,200],30)
+pygame.draw.circle(screen,"chartreuse3",[300,600],60)
+pygame.draw.circle(screen,"chartreuse3",[500,500],60)
+pygame.draw.circle(screen,"chartreuse3",[800,600],60)
+pygame.draw.circle(screen,"chartreuse3",[1100,480],60)
 pygame.display.flip()
 pygame.time.wait(2000)
 
