9250587a4f828dcec771ebc97051291e0a88c555
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-05-04 16:21:18 -0400

    save please

--- ch10/final_project/main.py
+++ ch10/final_project/main.py
@@ -1,22 +1,32 @@
 import pygame
-import tkinter
+import tkinter as tk
+from tkinter import *
+# from landscape import land
 
-root = tk()
+def _init_(self):
+    root = Tk()
+    mylabel = Label(root,text= ("hello world "))
+    mylabel.pack()
+    go_button = tk.Button(self.window,text = "go")
+    
+
+
+
 
 # import your controller --> this part of the code will call pygame to come into the tkinter program 
 
 
 
-def main():
-    pygame.init() ok i think tkinter stuff goes here 
+# def main():
+#     pygame.init() ok i think tkinter stuff goes here 
     # Create an instance on your controller object
-    # Call your mainloop
+root.mainloop()
 
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 
 
 # https://codefather.tech/blog/if-name-main-python/
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
