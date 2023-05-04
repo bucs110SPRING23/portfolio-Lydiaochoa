e25222bd554ce8f42cd49b8c7e4a96280ca0c820
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-05-04 09:13:22 -0400

    commit

--- ch10/exercises/Finaldraft.py
+++ ch10/exercises/Finaldraft.py
@@ -57,20 +57,20 @@ class frogs():
 
 if __name__ == "__main__":
     main()
-# pygame.init
-# w = 10000
-# h = 1000
-# screen = pygame.display.set_mode((w,h))
-# screencords = pygame.display.get_window_size()
-# pygame.event.get() 
-# screen.fill("chartreuse4")
-# pygame.display.flip() 
+pygame.init
+w = 10000
+h = 1000
+screen = pygame.display.set_mode((w,h))
+screencords = pygame.display.get_window_size()
+pygame.event.get() 
+screen.fill("chartreuse4")
+pygame.display.flip() 
 
-# class river:
-#     pygame.draw.rect(screen,"cadetblue2",(0,500,w,400))
-#     pygame.display.flip()
-#     pygame.time.wait(2000)
+class river:
+    pygame.draw.rect(screen,"cadetblue2",(0,400,w,400))
+    pygame.display.flip()
+    pygame.time.wait(2000)
 
-# class frog:
-#     player = pygame.draw.circle()
+class frog:
+    player = pygame.draw.circle()
     
