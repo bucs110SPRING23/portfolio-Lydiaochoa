b7ca7d2a59684ff1644888019d9c6d6747e18549
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-05-04 13:36:46 -0400

    saved

--- ch10/final_project/main.py
+++ ch10/final_project/main.py
@@ -1,16 +1,22 @@
 import pygame
+import tkinter
 
-# import your controller
+root = tk()
+
+# import your controller --> this part of the code will call pygame to come into the tkinter program 
+
 
 
 def main():
-    pygame.init()
+    pygame.init() ok i think tkinter stuff goes here 
     # Create an instance on your controller object
     # Call your mainloop
 
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 
+
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == "__main__":
     main()
+
