72fa2bf5f76e8c9a400c3c74d44cd1b8c57fc382
Lydia Ochoa <lydiaochoa71@gmail.com>
2023-05-04 22:10:12 -0400

    save

--- ch10/final_project/main.py
+++ ch10/final_project/main.py
@@ -13,27 +13,24 @@ def main():
     # Start the GUI event loop
     program.window.mainloop()
 
-class CounterProgram:
+class myProgram:
 
     def __init__(self):
         self.window = tk.Tk()
-        self.my_counter = None  # All attributes should be initialize in init
+        self.my_button = None  # All attributes should be initialize in init
         self.create_widgets()
 
     def create_widgets(self):
-        self.my_counter = tk.Label(self.window, text="0")
+        self.my_counter = tk.Label(self.window, text="start game?")
         self.my_counter.grid(row=0, column=0)
 
-        increment_button = tk.Button(self.window, text="Add 1 to counter")
-        increment_button.grid(row=1, column=0)
-        increment_button['command'] = self.increment_counter
+        player_button = tk.Button(self.window, text="yes")
+        player_button.grid(row=1, column=0)
+        player_button['command'] = self.increment_counter
 
         quit_button = tk.Button(self.window, text="Quit")
         quit_button.grid(row=2, column=0)
         quit_button['command'] = self.window.destroy
-
-    def increment_counter(self):
-        self.my_counter['text'] = str(int(self.my_counter['text']) + 1)
 
 if __name__ == "__main__":
     main()
@@ -48,8 +45,8 @@ root.mainloop()
 
 
 
-# https://codefather.tech/blog/if-name-main-python/
-if __name__ == "__main__":
-   from landscape import land()
-   main()
+# # https://codefather.tech/blog/if-name-main-python/
+# if __name__ == "__main__":
+#    from landscape import land()
+#    main()
 
